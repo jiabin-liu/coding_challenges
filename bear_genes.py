ef15b96f4daaf0d0387bcef593500ddf1d6e74cf
Jiabin <jiabin@berkeley.edu>
2017-09-27 12:24:04 -0700

    final version bear_genes

--- bear_genes.py
+++ bear_genes.py
@@ -1,7 +1,8 @@
 import fileinput
+from collections import defaultdict, deque
 lines=[line.strip() for line in fileinput.input()]
-N, genes = int(lines[0]), list(lines[1])
-from collections import defaultdict
+N, genes = int(lines[0]), lines[1]
+
 letters = defaultdict(int)
 for g in genes:
     letters[g]+=1
@@ -10,10 +11,15 @@ for k,v in letters.items():
     v = v-N/4
     if v > 0:
         letter_toomany[k] = v
+        
+if not letter_toomany:
+    print(0)
+    exit()
+    
 substring_min = int(sum(letter_toomany.values()))
 
 p1, p2 = 0, substring_min #pointers to the sub-string we examine
-sub_now = genes[p1:p2]
+sub_now = deque(genes[p1:p2])
 min_len = N
 
 dict_sub=defaultdict(int)
@@ -29,13 +35,14 @@ def ismatch(dictsub):
     return True
 
 while True:
-    sub_now = genes[p1:p2]
     if ismatch(dict_sub): #substring satisfy
-        min_len = min(min_len,len(sub_now))
+        min_len = min(min_len, p2-p1)
         dict_sub[genes[p1]] -= 1
+        A=sub_now.popleft()
         p1 += 1 #move left pointer forward
     elif p2 < N: #substring not satisfy; can extend right-end
         dict_sub[genes[p2]] += 1
+        sub_now.append(genes[p2])
         p2 += 1
     else: #substring not satisfy; cannot extend right-end
         break
